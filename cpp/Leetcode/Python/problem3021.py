3b547ba91783638329077fc7f366544a5289db08
M D Aravind <131228997+mdaravind123@users.noreply.github.com>
2024-01-28 16:15:26 +0530

    10th commit

---
+++ cpp/Leetcode/Python/problem3021.py
@@ -0,0 +1,7 @@
+import math as m
+
+class Solution:
+    def flowerGame(self, n: int, m: int) -> int:
+        return m.floor((n*m)/2)
+                
+        
---
+++ cpp/Leetcode/c++/problem205.cpp
@@ -0,0 +1,9 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+  string s="badc";
+  string t="baba";
+  
+  return 0;
+}

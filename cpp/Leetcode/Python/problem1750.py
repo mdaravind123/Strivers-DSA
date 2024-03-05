200a3a4a6868d6a05730085f0ceed84e9fa452cc
M D Aravind <131228997+mdaravind123@users.noreply.github.com>
2024-03-05 19:14:21 +0530

    update: solve 5-3-24 daily challenge question

---
+++ cpp/Leetcode/Python/problem1750.py
@@ -0,0 +1,11 @@
+class Solution:
+    def minimumLength(self, s: str) -> int:
+        left=0
+        right=len(s)-1
+        while(left<right and s[left]==s[right]):
+            prev=s[left]
+            while(left<=right and s[left]==prev):
+                left+=1
+            while(right>=left and s[right]==prev):
+                right-=1
+        return right-left+1
